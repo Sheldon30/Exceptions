5866d4ecb6e844cf9745184281d8e2ce01ccb36b
Sheldon <aneln700@gmail.com>
2023-08-26 19:36:34 +0300

    Work

---
+++ src/test/java/ProductRepositoryTest.java
@@ -0,0 +1,34 @@
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class ProductRepositoryTest {
+
+    Product item1 = new Product(11, "Хлеб", 45);
+    Product item2 = new Product(22, "Рис", 85);
+    Product item3 = new Product(3, "Торт", 555);
+
+    @Test
+    public void test() {
+        ShopRepository repo = new ShopRepository();
+        repo.add(item1);
+        repo.add(item2);
+        repo.add(item3);
+
+        repo.remove(22);
+        Product[] expected = {item1, item3};
+        Product[] actual = repo.findAll();
+
+        Assertions.assertArrayEquals(expected, actual);
+    }
+    @Test
+    public void testNegative() {
+        ShopRepository repo = new ShopRepository();
+        repo.add(item1);
+        repo.add(item2);
+        repo.add(item3);
+
+        Assertions.assertThrows(NotFoundException.class, () -> {
+            repo.remove(15);
+        });
+    }
+}
